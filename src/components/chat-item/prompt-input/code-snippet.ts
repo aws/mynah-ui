25da3320b550a032e6f46034e862c1bc504ee2d5
Mike Fan <147854708+mk-fan@users.noreply.github.com>
2023-11-10 16:27:00 -0800

    Refactor chat-prompt-input and fix the remaining char count (#24)

--- src/components/chat-item/prompt-input/code-snippet.ts
+++ src/components/chat-item/prompt-input/code-snippet.ts
@@ -1,14 +1,9 @@
 import { DomBuilder, ExtendedHTMLElement } from '../../../helper/dom';
 import { MynahUITabsStore } from '../../../helper/tabs-store';
-import { MynahUIGlobalEvents } from '../../../helper/events';
-import { MynahEventNames } from '../../../static';
 import { CodeSnippetWidget } from './code-snippet-widget';
-import { MAX_USER_INPUT } from '../chat-prompt-input';
 
 export interface ICodeSnippetProps {
   tabId: string;
-  onCodeSnippetAdd?: (codeSnippet: string) => void;
-  onCodeSnippetRemove?: () => void;
 }
 
 export interface ISelectCodeSnippetEvent {
@@ -17,59 +12,42 @@ export interface ISelectCodeSnippetEvent {
 }
 
 export class CodeSnippet {
-  private readonly props: ICodeSnippetProps;
-  render: ExtendedHTMLElement;
-  availableChars: number = MAX_USER_INPUT - 100;
+  private readonly _props: ICodeSnippetProps;
+  private readonly _render: ExtendedHTMLElement;
   constructor (props: ICodeSnippetProps) {
-    this.props = props;
+    this._props = props;
 
-    MynahUIGlobalEvents.getInstance().addListener(MynahEventNames.ADD_CODE_SNIPPET, (data: ISelectCodeSnippetEvent) => {
-      MynahUITabsStore.getInstance().getTabDataStore(data.tabId).updateStore({
-        selectedCodeSnippet: data.selectedCodeSnippet?.substring(0, this.availableChars),
-      });
-      if (this.props.onCodeSnippetAdd != null) {
-        this.props.onCodeSnippetAdd(data.selectedCodeSnippet?.substring(0, this.availableChars) as string);
-      }
-    });
-
-    MynahUIGlobalEvents.getInstance().addListener(MynahEventNames.REMOVE_CODE_SNIPPET, (tabId: string) => {
-      MynahUITabsStore.getInstance().getTabDataStore(tabId).updateStore({
-        selectedCodeSnippet: undefined,
-      });
-      if (this.props.onCodeSnippetRemove != null) {
-        this.props.onCodeSnippetRemove();
-      }
-    });
-
-    MynahUITabsStore.getInstance()
-      .getTabDataStore(this.props.tabId)
-      .subscribe('selectedCodeSnippet', (selectedCodeSnippet: string) => {
-        this.render.clear();
-        if (selectedCodeSnippet !== undefined && selectedCodeSnippet !== '') {
-          this.render.insertChild(
-            'afterbegin',
-            new CodeSnippetWidget({
-              tabId: props.tabId,
-              markdownText: selectedCodeSnippet,
-            }).render
-          );
-        }
-      });
-
-    this.render = DomBuilder.getInstance().build({
+    this._render = DomBuilder.getInstance().build({
       type: 'div',
       classNames: [ 'outer-container' ],
       persistent: true,
     });
   }
 
-  public readonly clear = (): void => {
-    this.render.clear();
-    if (this.props.onCodeSnippetRemove !== undefined) {
-      this.props.onCodeSnippetRemove();
+  public get render (): ExtendedHTMLElement {
+    return this._render;
+  }
+
+  public readonly updateSelectedCodeSnippet = (selectedCodeSnippet: string | undefined): void => {
+    this._render.clear();
+    if (selectedCodeSnippet !== undefined && selectedCodeSnippet !== '') {
+      this._render.insertChild(
+        'afterbegin',
+        new CodeSnippetWidget({
+          tabId: this._props.tabId,
+          markdownText: selectedCodeSnippet,
+        }).render
+      );
     }
-    MynahUITabsStore.getInstance().getTabDataStore(this.props.tabId).updateStore({
-      selectedCodeSnippet: '',
+    MynahUITabsStore.getInstance().getTabDataStore(this._props.tabId).updateStore({
+      selectedCodeSnippet,
+    });
+  };
+
+  public readonly clear = (): void => {
+    this._render.clear();
+    MynahUITabsStore.getInstance().getTabDataStore(this._props.tabId).updateStore({
+      selectedCodeSnippet: undefined,
     });
   };
 }
---
+++ src/components/chat-item/prompt-input/prompt-text-input.ts
@@ -0,0 +1,126 @@
+import { DomBuilder, ExtendedHTMLElement } from '../../../helper/dom';
+import { MynahUITabsStore } from '../../../helper/tabs-store';
+import { MAX_USER_INPUT } from '../chat-prompt-input';
+
+export interface IPromptTextInputProps {
+  tabId: string;
+  onKeydown: (e: KeyboardEvent) => void;
+  onInput?: (e: KeyboardEvent) => void;
+}
+
+export class PromptTextInput {
+  private readonly _props: IPromptTextInputProps;
+  private readonly _render: ExtendedHTMLElement;
+  private readonly _promptTextInputSizer: ExtendedHTMLElement;
+  private readonly _promptTextInput: ExtendedHTMLElement;
+  constructor (props: IPromptTextInputProps) {
+    this._props = props;
+
+    const initialDisabledState = MynahUITabsStore.getInstance().getTabDataStore(this._props.tabId).getValue('promptInputDisabledState') as boolean;
+
+    this._promptTextInputSizer = DomBuilder.getInstance().build({
+      type: 'span',
+      classNames: [ 'mynah-chat-prompt-input', 'mynah-chat-prompt-input-sizer' ],
+    });
+
+    this._promptTextInput = DomBuilder.getInstance().build({
+      type: 'textarea',
+      classNames: [ 'mynah-chat-prompt-input' ],
+      attributes: {
+        ...(initialDisabledState ? { disabled: 'disabled' } : {}),
+        tabindex: '1',
+        rows: '1',
+        maxlength: MAX_USER_INPUT.toString(),
+        type: 'text',
+        placeholder: MynahUITabsStore.getInstance().getTabDataStore(this._props.tabId).getValue('promptInputPlaceholder'),
+        value: '',
+      },
+      events: {
+        keydown: this._props.onKeydown,
+        input: (e: KeyboardEvent) => {
+          // Set the appropriate prompt input height
+          this.updatePromptTextInputSizer();
+          // Additional handler
+          if (this._props.onInput !== undefined) {
+            this._props.onInput(e);
+          }
+        },
+        focus: () => {
+          this.render.addClass('input-has-focus');
+        },
+        blur: () => {
+          this.render.removeClass('input-has-focus');
+        }
+      },
+    });
+
+    this._render = DomBuilder.getInstance().build({
+      type: 'div',
+      classNames: [ 'mynah-chat-prompt-input-inner-wrapper', 'no-text' ],
+      children: [
+        this._promptTextInputSizer,
+        this._promptTextInput,
+      ]
+    });
+
+    MynahUITabsStore.getInstance().getTabDataStore(this._props.tabId).subscribe('promptInputDisabledState', (isDisabled: boolean) => {
+      if (isDisabled) {
+        this._promptTextInput.setAttribute('disabled', 'disabled');
+      } else {
+        // Enable the input field and focus on it
+        this._promptTextInput.removeAttribute('disabled');
+        this._promptTextInput.focus();
+      }
+    });
+  }
+
+  get render (): ExtendedHTMLElement {
+    return this._render;
+  }
+
+  private readonly updatePromptTextInputSizer = (): void => {
+    if (this._promptTextInput.value.trim() !== '') {
+      this._render.removeClass('no-text');
+    } else {
+      this._render.addClass('no-text');
+    }
+    this._promptTextInputSizer.innerHTML = this._promptTextInput.value.replace(/\n/g, '</br>&nbsp;');
+  };
+
+  public readonly clear = (): void => {
+    this._promptTextInputSizer.innerHTML = '';
+    this.updateTextInputValue('');
+    const defaultPlaceholder = MynahUITabsStore.getInstance().getTabDataStore(this._props.tabId).getValue('promptInputPlaceholder');
+    this.updateTextInputPlaceholder(defaultPlaceholder);
+    this._render.addClass('no-text');
+  };
+
+  public readonly focus = (): void => {
+    this._promptTextInput.focus();
+    this.updateTextInputValue('');
+  };
+
+  public readonly getTextInputValue = (): string => {
+    return this._promptTextInput.value;
+  };
+
+  public readonly updateTextInputValue = (value: string): void => {
+    this._promptTextInput.value = value;
+  };
+
+  public readonly updateTextInputMaxLength = (maxLength: number): void => {
+    this._promptTextInput.update({
+      attributes: {
+        maxlength: maxLength.toString(),
+      }
+    });
+  };
+
+  public readonly updateTextInputPlaceholder = (text: string): void => {
+    this._promptTextInput.update({
+      attributes: {
+        placeholder: text,
+      }
+    });
+  };
+}
