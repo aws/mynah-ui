225244764b29871e52ffd795f9e7eb6aa1ac6236
Dogus Atasoy <dogusata@amazon.com>
2024-04-24 16:44:30 +0200

    reverted the maxTabs for original example

--- example/src/main.ts
+++ example/src/main.ts
@@ -42,7 +42,7 @@ export const createMynahUI = (initialData?: MynahUIDataModel): MynahUI => {
     rootSelector: '#amzn-mynah-website-wrapper',
     defaults: mynahUIDefaults,
     config: {
-      maxTabs: 1,
+      maxTabs: 5,
       autoFocus: true,
       tabBarButtons: [
         {
