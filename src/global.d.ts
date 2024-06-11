79597d0a99e6bcf3c6700f81ba9e999221f4df35
Dogus Atasoy <dogusata@amazon.com>
2024-06-11 13:42:12 +0200

    removed unnecessary module definition

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,9 +2,3 @@ declare module '*.svg' {
     const content:string;
     export default content;
 }
-declare module '*MynahUIReactComponents.umd.js' {
-    const MynahUICard:MynahUICard;
-    const MynahUIButton:MynahUIButton;
-    export const MynahUICard;
-    export const MynahUIButton;
-}
