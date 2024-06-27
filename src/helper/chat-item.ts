1372e40ce34dc49bf362b9faf5acb5bef4f90e30
Dogus Atasoy <dogusata@amazon.com>
2024-06-28 00:50:58 +0200

    removed footer check to avoid showing no content but footer only cards

--- src/helper/chat-item.ts
+++ src/helper/chat-item.ts
@@ -3,7 +3,6 @@ import { ChatItem } from '../static';
 export const chatItemHasContent = (chatItem: Partial<ChatItem>): boolean => (
   (chatItem.body != null && chatItem.body !== '') ||
 chatItem.fileList != null ||
-chatItem.footer != null ||
 chatItem.formItems != null ||
 chatItem.customRenderer != null ||
 chatItem.buttons != null);
