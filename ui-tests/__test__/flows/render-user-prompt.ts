dda54c02cbdfb3fd1f0587a884819ea5b5345098
Dogus Atasoy <dogusata@amazon.com>
2024-09-27 01:39:04 +0200

    updated snapshots

--- ui-tests/__test__/flows/render-user-prompt.ts
+++ ui-tests/__test__/flows/render-user-prompt.ts
@@ -2,7 +2,7 @@ import { Page } from 'playwright/test';
 import { getSelector, waitForAnimationEnd } from '../helpers';
 import testIds from '../../../src/helper/test-ids';
 
-export const renderUserPrompt = async (page: Page): Promise<void> => {
+export const renderUserPrompt = async (page: Page, skipScreenshots?: boolean): Promise<void> => {
   await page.locator(`${getSelector(testIds.prompt.input)}`).fill('This is a user Prompt');
   await page.locator(`${getSelector(testIds.prompt.send)}`).click();
   const promptInput = await page.waitForSelector(`${getSelector(testIds.prompt.input)}`);
@@ -11,9 +11,12 @@ export const renderUserPrompt = async (page: Page): Promise<void> => {
   const userCardSelector = `${getSelector(testIds.chatItem.type.prompt)}`;
   const userCard = await page.waitForSelector(userCardSelector);
   expect(userCard).toBeDefined();
-  await waitForAnimationEnd(page, userCardSelector);
-  expect(await userCard.screenshot()).toMatchImageSnapshot();
-
   await waitForAnimationEnd(page);
+  await userCard.scrollIntoViewIfNeeded();
   expect(await promptInput.isDisabled()).toEqual(false);
+
+  if (skipScreenshots !== true) {
+    const chatItemsContainer = await page.waitForSelector(`${getSelector(testIds.chat.chatItemsContainer)}`);
+    expect(await chatItemsContainer.screenshot()).toMatchImageSnapshot();
+  }
 };
