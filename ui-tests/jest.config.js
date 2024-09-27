51f962080cfd1472f5308662e4811df0f455384e
Dogus Atasoy <dogusata@amazon.com>
2024-09-27 09:44:33 +0200

    updated snapshots and font rendering

--- ui-tests/jest.config.js
+++ ui-tests/jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
         'jest-playwright': {
             browsers: ['webkit', 'chromium'],
             launchOptions: {
-                headless: true,
+                headless: false,
             }
         },
     },
