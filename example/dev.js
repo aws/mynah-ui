c7e8670232fdb6960cd31cb9532cf6bb3f97fb68
eugene yevhen andruszczenko <andruseu@amazon.com>
2024-10-02 17:29:41 -0400

    feat: improved command selector with scroll and scroll into view

--- example/dev.js
+++ example/dev.js
@@ -1,2 +1,12 @@
-import open from 'open';
-await open('./dist/index.html');
+// Dynamic import handling
+const openFile = async () => {
+    try {
+        // Use dynamic import to load the ES module
+        const open = (await import('open')).default;
+        await open('./dist/index.html');
+    } catch (err) {
+        console.error('Error opening file:', err);
+    }
+};
+
+openFile();
