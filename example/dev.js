78c215ba74df5355f31f9815e3a059ff64f1b3d8
eugene yevhen andruszczenko <andruseu@amazon.com>
2024-10-10 09:41:58 -0400

    fix: enable scroll in text input when no text

--- example/dev.js
+++ example/dev.js
@@ -1,2 +1,11 @@
-import open from 'open';
-await open('./dist/index.html');
+const openFile = async () => {
+  try {
+    // Use dynamic import to load the ES module
+    const open = (await import('open')).default;
+    await open('./dist/index.html');
+  } catch (err) {
+    console.error('Error opening file:', err);
+  }
+};
+
+openFile();
