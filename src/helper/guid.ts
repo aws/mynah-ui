edb8c2a0d66a435f5081800d80d06cbfd1b063d8
Jurre de Ruiter <github@jurre.me>
2025-01-08 12:10:51 +0100

    feat: improved random UID generation through crypto module

--- src/helper/guid.ts
+++ src/helper/guid.ts
@@ -4,7 +4,5 @@
  */
 
 export const generateUID = (): string => {
-  const firstPart: number = (Math.random() * 46656) | 0;
-  const secondPart: number = (Math.random() * 46656) | 0;
-  return `000${firstPart.toString(36)}`.slice(-3) + `000${secondPart.toString(36)}`.slice(-3);
+  return crypto.randomUUID();
 };
