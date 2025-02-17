a488f92082462f85406d2b50fa22a2de4299e620
Jurre de Ruiter <github@jurre.me>
2025-02-17 14:57:15 +0100

    Updated default example app answer to include file in footer instead of header again

--- example/src/config.ts
+++ example/src/config.ts
@@ -146,7 +146,7 @@ export const mynahUIDefaults: Partial<MynahUITabStoreTab> = {
       {
         type: ChatItemType.ANSWER,
         body: WelcomeMessage,
-        header: {
+        footer: {
           fileList: {
             rootFolderTitle: undefined,
             fileTreeTitle: '',
