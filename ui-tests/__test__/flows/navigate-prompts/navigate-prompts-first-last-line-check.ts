fa3f058ea96c335c53e8eac8bb82df7cfaeace41
Jurre de Ruiter <github@jurre.me>
2025-03-04 16:11:58 +0100

    Removed console log from e2e test

--- ui-tests/__test__/flows/navigate-prompts/navigate-prompts-first-last-line-check.ts
+++ ui-tests/__test__/flows/navigate-prompts/navigate-prompts-first-last-line-check.ts
@@ -17,9 +17,6 @@ export const navigatePromptsFirstLastLineCheck = async (page: Page, skipScreensh
   await waitForAnimationEnd(page);
 
   // The input should start as the input with two lines
-  console.log((await promptInput.innerText()) === secondPrompt);
-  console.log(await promptInput.innerText());
-
   expect(await promptInput.innerText()).toBe(secondPrompt);
 
   // Input should remain the same as it is multiline
