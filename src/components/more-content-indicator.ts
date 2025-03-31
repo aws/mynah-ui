9581c82ce5e86aaea929b0e62a3b59cff5d76d2f
Dogus Atasoy <dogusata@amazon.com>
2025-03-31 15:43:49 +0200

    feature: converted more-content-indicator to reusable component

---
+++ src/components/more-content-indicator.ts
@@ -0,0 +1,56 @@
+/*!
+ * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// eslint-disable @typescript-eslint/restrict-template-expressions
+import { DomBuilder, ExtendedHTMLElement } from '../helper/dom';
+import { Icon, MynahIcons, MynahIconsType } from './icon';
+import '../styles/components/_more-content-indicator.scss';
+import { Button } from './button';
+
+interface MoreContentIndicatorProps {
+  icon?: MynahIcons | MynahIconsType;
+  border?: boolean;
+  testId?: string;
+  onClick: () => void;
+}
+export class MoreContentIndicator {
+  render: ExtendedHTMLElement;
+  private props: MoreContentIndicatorProps;
+  private button: Button;
+  private readonly uid: string;
+  private readonly icon: ExtendedHTMLElement;
+  constructor (props: MoreContentIndicatorProps) {
+    this.props = props;
+    this.button = this.getButton();
+    this.render = DomBuilder.getInstance().build({
+      type: 'div',
+      classNames: [ 'more-content-indicator' ],
+      testId: props.testId,
+      children: [
+        this.button.render
+      ]
+    });
+  }
+
+  private readonly getButton = (): Button => {
+    return new Button({
+      icon: new Icon({ icon: this.props.icon ?? MynahIcons.SCROLL_DOWN }).render,
+      primary: false,
+      fillState: 'hover',
+      border: this.props.border !== false,
+      onClick: this.props.onClick
+    });
+  };
+
+  public update = (props: Partial<MoreContentIndicatorProps>): void => {
+    this.props = {
+      ...this.props,
+      ...props
+    };
+    const newButton = this.getButton();
+    this.button.render.replaceWith(newButton.render);
+    this.button = newButton;
+  };
+}
