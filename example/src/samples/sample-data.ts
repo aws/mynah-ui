0c97374ea48be57e0f698c3d5b3fa02c697fd516
Dung Dong <dungdong@amazon.com>
2025-06-30 14:15:24 -0700

    fix: revert sample-data welcome page; add this to samples of card-with-headers

--- example/src/samples/sample-data.ts
+++ example/src/samples/sample-data.ts
@@ -504,54 +504,6 @@ _Generate code, scan for issues, and more._`,
                     },
                 ],
             },
-            {
-                fullWidth: true,
-                padding: false,
-                type: ChatItemType.ANSWER,
-                header: {
-                    body: 'Shell',
-                    status: {
-                        position: 'left',
-                        icon: MynahIcons.WARNING,
-                        status: 'warning',
-                        description: 'This command may cause\nsignificant data loss or damage.',
-                    },
-                    buttons: [
-                        {
-                            status: 'clear',
-                            icon: 'play',
-                            text: 'Run',
-                            id: 'run-bash-command',
-                        },
-                        {
-                            status: 'dimmed-clear',
-                            icon: 'cancel',
-                            text: 'Reject',
-                            id: 'reject-bash-command',
-                        },
-                    ],
-                },
-                body: 'mkdir -p src/ lalalaaaa',
-                dropdownList: {
-                    title: 'Destructive commands',
-                    messageId: "1",
-                    tabId: "hello",
-                    titleIcon: MynahIcons.WARNING,
-                    description: 'Configure run behavior for this session. To adjust global settings, go to',
-                    descriptionLink: {
-                        id: "button-id",
-                        text: 'Auto-approve settings',
-                    },
-                    options: [
-                    { id: 'option1', label: 'Ask to Run', selected: true, value: 'Destructive' },
-                    { id: 'option2', label: 'Auto run', value: 'Destructive' },
-                    ],
-                    onChange: (selectedOptions) => {
-                        console.log('Selected options:', selectedOptions);
-                    }
-            },
-                codeBlockActions: { copy: null, 'insert-to-cursor': null },
-            }
         ],
         promptInputLabel: 'Or, start a chat',
         promptInputPlaceholder: 'Type your question',
@@ -1774,7 +1726,54 @@ mkdir -p src/ lalalaaaa
 `,
         codeBlockActions: { copy: null, 'insert-to-cursor': null },
     },
-
+    {
+        fullWidth: true,
+        padding: false,
+        type: ChatItemType.ANSWER,
+        header: {
+            body: 'Shell',
+            status: {
+                position: 'left',
+                icon: MynahIcons.WARNING,
+                status: 'warning',
+                description: 'This command may cause\nsignificant data loss or damage.',
+            },
+            buttons: [
+                {
+                    status: 'clear',
+                    icon: 'play',
+                    text: 'Run',
+                    id: 'run-bash-command',
+                },
+                {
+                    status: 'dimmed-clear',
+                    icon: 'cancel',
+                    text: 'Reject',
+                    id: 'reject-bash-command',
+                },
+            ],
+        },
+        body: 'mkdir -p src/ lalalaaaa',
+        dropdownList: {
+            title: 'Destructive commands',
+            messageId: "1",
+            tabId: "hello",
+            titleIcon: MynahIcons.WARNING,
+            description: 'Configure run behavior for this session. To adjust global settings, go to',
+            descriptionLink: {
+                id: "button-id",
+                text: 'Auto-approve settings',
+            },
+            options: [
+            { id: 'option1', label: 'Ask to Run', selected: true, value: 'Destructive' },
+            { id: 'option2', label: 'Auto run', value: 'Destructive' },
+            ],
+            onChange: (selectedOptions) => {
+                console.log('Selected options:', selectedOptions);
+            }
+    },
+        codeBlockActions: { copy: null, 'insert-to-cursor': null },
+    },
     {
         fullWidth: true,
         padding: false,
